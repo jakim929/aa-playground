505d139590299c3c5d16d60f2f4f1ffdf95f0a44
James Kim <jakim929@gmail.com>
2023-11-17 12:23:22 -0500

    use ddp to deploy Kernel

--- packages/contracts/README.md
+++ packages/contracts/README.md
@@ -1,1 +1,30 @@
 # Contracts
+
+Deploys contracts required to use [Kernel smart accounts](https://github.com/zerodevapp/kernel)
+
+## Deployment addresses
+
+The following is deployed using a [deterministic deployment proxy](https://github.com/Arachnid/deterministic-deployment-proxy) with salt
+
+ `bytes32 ddSalt = '31337'`
+
+### KernelFactory
+```sh
+0xb0392f89D211f36Bd0A89F269b2146cA82d49060
+```
+
+
+### KernelFactory configuration
+```sh
+# OWNER
+# derived from 'test test test test test test test test test test test junk'
+0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+
+# ENTRYPOINT STAKE AMOUNT
+1 ETH
+```
+
+### Kernel implementation
+```sh
+0x8DfebcdA1Af688C439d9b37D828C0668c8B791f6
+```
--- packages/contracts/script/DeployKernel.s.sol
+++ packages/contracts/script/DeployKernel.s.sol
@@ -2,24 +2,32 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 import { KernelFactory } from "kernel/src/factory/KernelFactory.sol";
 import { Kernel } from "kernel/src/Kernel.sol";
 import { IEntryPoint } from "I4337/interfaces/IEntryPoint.sol";
 
 
 contract DeployKernelFactoryScript is Script {
-    function setUp() public {}
+    function setUp() public {
+
+    }
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         address ownerAddress = vm.envAddress("OWNER_ADDRESS");
         IEntryPoint entryPoint = IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789);
+        bytes32 ddSalt = "31337";
 
         vm.startBroadcast(deployerPrivateKey);
 
-        KernelFactory kernelFactory = new KernelFactory(ownerAddress, IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789));
+        KernelFactory kernelFactory = new KernelFactory{ salt: ddSalt }(ownerAddress, entryPoint);
 
-        Kernel kernel = new Kernel(entryPoint);
+        console.log("KernelFactory deployed to: %s", address(kernelFactory));
+
+        Kernel kernel = new Kernel{ salt: ddSalt }(entryPoint);
+
+        console.log("Kernel impl deployed to: %s", address(kernel));
 
         kernelFactory.setImplementation(address(kernel), true);
 
