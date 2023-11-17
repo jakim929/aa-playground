072566011087edb6b34dfca30a622c7ada0ad556
James Kim <jakim929@gmail.com>
2023-11-17 11:54:57 -0500

    add kernel setup script

--- packages/contracts/script/DeployKernel.s.sol
+++ packages/contracts/script/DeployKernel.s.sol
@@ -3,19 +3,27 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import { KernelFactory } from "kernel/src/factory/KernelFactory.sol";
+import { Kernel } from "kernel/src/Kernel.sol";
 import { IEntryPoint } from "I4337/interfaces/IEntryPoint.sol";
 
 
-contract DeployCounterScript is Script {
+contract DeployKernelFactoryScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         address ownerAddress = vm.envAddress("OWNER_ADDRESS");
+        IEntryPoint entryPoint = IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789);
 
         vm.startBroadcast(deployerPrivateKey);
 
         KernelFactory kernelFactory = new KernelFactory(ownerAddress, IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789));
+
+        Kernel kernel = new Kernel(entryPoint);
+
+        kernelFactory.setImplementation(address(kernel), true);
+
+        kernelFactory.addStake{ value: 1 ether }(1000);
 
         vm.stopBroadcast();
     }
