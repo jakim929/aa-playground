3c029cb3a49485bbdba72d68f9ddbd64fb45fbaa
James Kim <jakim929@gmail.com>
2023-11-17 10:46:39 -0500

    fix remappings

---
+++ packages/contracts/script/DeployKernelFactory.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import "forge-std/Script.sol";
+import { KernelFactory } from "kernel/src/KernelFactory.sol";
+
+contract DeployCounterScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address ownerAddress = vm.envAddress("OWNER_ADDRESS");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        KernelFactory kernelFactory = new KernelFactory(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789);
+
+        vm.stopBroadcast();
+    }
+}
